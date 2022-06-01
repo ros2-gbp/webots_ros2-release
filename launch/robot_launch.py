c40a3364b1555513df54f8a264ec3761116e3c89
ad-daniel <daniel.dias@epfl.ch>
2022-06-01 21:02:58 +0200

    Rebase from 'debian/foxy/webots_ros2_universal_robot'

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -45,9 +45,11 @@ def generate_launch_description():
     controller_manager_timeout = ['--controller-manager-timeout', '100']
     controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
 
+    use_deprecated_spawner_py = 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'foxy'
+
     trajectory_controller_spawner = Node(
         package='controller_manager',
-        executable='spawner.py',
+        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
         output='screen',
         prefix=controller_manager_prefix,
         arguments=['ur_joint_trajectory_controller'] + controller_manager_timeout,
@@ -55,7 +57,7 @@ def generate_launch_description():
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
-        executable='spawner.py',
+        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
         output='screen',
         prefix=controller_manager_prefix,
         arguments=['ur_joint_state_broadcaster'] + controller_manager_timeout,
@@ -68,6 +70,7 @@ def generate_launch_description():
         parameters=[
             {'robot_description': robot_description},
             {'use_sim_time': True},
+            {'set_robot_state_publisher': True},
             ros2_control_params
         ]
     )

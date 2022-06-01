0516174e21bd519cac25403995cfb0cdceb26557
ad-daniel <daniel.dias@epfl.ch>
2022-06-01 21:57:00 +0200

    Rebase from 'upstream'

--- webots_ros2_importer/webots_ros2_importer/urdf2webots/tests/test_export.py
+++ webots_ros2_importer/webots_ros2_importer/urdf2webots/tests/test_export.py
@@ -1,8 +1,12 @@
 """Test module of the urdf2webots script."""
+import io
 import os
+import pathlib
+import shutil
 import sys
 import unittest
-import shutil
+
+from urdf2webots.importer import convertUrdfContent, convertUrdfFile
 
 testDirectory = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 sourceDirectory = os.path.join(testDirectory, 'sources')
@@ -10,15 +14,17 @@ resultDirectory = os.path.join(testDirectory, 'results')
 expectedDirectory = os.path.join(testDirectory, 'expected')
 urdf2webotsPath = os.path.abspath(os.path.join(testDirectory, '..', 'urdf2webots/importer.py'))
 
-modelPaths = [
+human_file_path = os.path.join(sourceDirectory, 'gait2392_simbody/urdf/human.urdf')
+
+modelPathsProto = [
     {
         'input': os.path.join(sourceDirectory, 'motoman/motoman_sia20d_support/urdf/sia20d.urdf'),
         'output': os.path.join(resultDirectory, 'MotomanSia20d.proto'),
         'expected': [os.path.join(expectedDirectory, 'MotomanSia20d.proto')],
-        'arguments': '--multi-file --static-base --tool-slot=tool0 --rotation="1 0 0 0" --init-pos="[0.1, -0.1, 0.2]"'
+        'arguments': '--multi-file --tool-slot=tool0 --rotation="1 0 0 0" --init-pos="[0.1, -0.1, 0.2]"'
     },
     {
-        'input': os.path.join(sourceDirectory, 'gait2392_simbody/urdf/human.urdf'),
+        'input': human_file_path,
         'output': os.path.join(resultDirectory, 'Human.proto'),
         'expected': [os.path.join(expectedDirectory, 'Human.proto')],
         'arguments': ''
@@ -27,7 +33,33 @@ modelPaths = [
         'input': os.path.join(sourceDirectory, 'kuka_lbr_iiwa_support/urdf/model.urdf'),
         'output': os.path.join(resultDirectory, 'KukaLbrIiwa14R820.proto'),
         'expected': [os.path.join(expectedDirectory, 'KukaLbrIiwa14R820.proto')],
-        'arguments': '--box-collision --static-base --tool-slot=tool0 --rotation="1 0 0 -1.5708"'
+        'arguments': '--box-collision --tool-slot=tool0 --rotation="1 0 0 -1.5708"'
+    },
+    {
+        'input': os.path.join(sourceDirectory, 'RobotWithDummyLink.urdf'),
+        'output': os.path.join(resultDirectory, 'RobotWithDummyLink.proto'),
+        'expected': [os.path.join(expectedDirectory, 'RobotWithDummyLink.proto')],
+        'arguments': ''
+    }
+]
+
+modelContentProto = [
+    {
+        'input': pathlib.Path(human_file_path).read_text(),
+        'output': os.path.join(resultDirectory, 'Human.proto'),
+        'expected': [os.path.join(expectedDirectory, 'Human.proto')],
+        'relativePathPrefix': '/home/runner/work/urdf2webots/urdf2webots/tests/sources/gait2392_simbody/urdf',
+    }
+]
+
+modelPathsRobotString = [
+    {
+        'input': os.path.join(sourceDirectory, 'kuka_lbr_iiwa_support/urdf/model.urdf'),
+        'output': os.path.join(resultDirectory, 'KukaLbrIiwa14R820.txt'),
+        'expected': [os.path.join(expectedDirectory, 'KukaLbrIiwa14R820.txt')],
+        'robotName': 'kuka',
+        'translation': '0 0 2',
+        'rotation': '1 0 0 -1.5708'
     }
 ]
 
@@ -36,7 +68,7 @@ def fileCompare(file1, file2):
     """Compare content of two files."""
     with open(file1) as f1, open(file2) as f2:
         for i, (line1, line2) in enumerate(zip(f1, f2)):
-            if line1.startswith('# Extracted from:') and line2.startswith('# Extracted from:'):
+            if line1.startswith('# Extracted from') and line2.startswith('# Extracted from'):
                 # This line may differ.
                 continue
             elif line1.startswith('#VRML_SIM') and line2.startswith('#VRML_SIM'):
@@ -59,14 +91,48 @@ class TestScript(unittest.TestCase):
     def setUp(self):
         """Cleanup results directory."""
         shutil.rmtree(resultDirectory, ignore_errors=True)
+        os.makedirs(resultDirectory)
 
-    def test_script_produces_the_correct_result(self):
-        """Test that urdf2webots produces an expected result."""
-        for paths in modelPaths:
+    def testInputFileOutputProto(self):
+        """Test that urdf2webots produces an expected PROTO file using URDF file as input."""
+        print('Start tests with input "URDF file" and output "PROTO file"...')
+        for paths in modelPathsProto:
             command = ('%s %s --input=%s --output=%s %s' %
                        (sys.executable, urdf2webotsPath, paths['input'], paths['output'], paths['arguments']))
             retcode = os.system(command)
             self.assertEqual(retcode, 0, msg='Error when exporting "%s"' % (paths['input']))
+            for expected in paths['expected']:
+                self.assertTrue(fileCompare(expected.replace('expected', 'results'), expected),
+                                msg='Expected result mismatch when exporting "%s"' % paths['input'])
+
+    def testInputContentOutputProto(self):
+        """Test that urdf2webots produces an expected PROTO file using URDF content as input."""
+        print('Start tests with input "URDF content" and output "PROTO file"...')
+        for paths in modelContentProto:
+            convertUrdfContent(input=paths['input'], output=paths['output'], relativePathPrefix=paths['relativePathPrefix'])
+            for expected in paths['expected']:
+                self.assertTrue(fileCompare(expected.replace('expected', 'results'), expected),
+                                msg='Expected result mismatch when exporting input to "%s"' % paths['output'])
+
+    def testInputContentStdinOutputProto(self):
+        """Test that urdf2webots produces an expected PROTO file using URDF content in the stdin buffer as input."""
+        print('Start tests with input "URDF content" and output "PROTO file"...')
+        for paths in modelContentProto:
+            sys.stdin = io.StringIO(paths['input'])
+            convertUrdfFile(output=paths['output'], relativePathPrefix=paths['relativePathPrefix'])
+            for expected in paths['expected']:
+                self.assertTrue(fileCompare(expected.replace('expected', 'results'), expected),
+                                msg='Expected result mismatch when exporting input to "%s"' % paths['output'])
+
+    def testInputFileOutputRobotString(self):
+        """Test that urdf2webots produces an expected Robot node string using URDF file as input."""
+        print('Start tests with input "URDF file" and output "Robot node strings"...')
+        for paths in modelPathsRobotString:
+            robot_string = convertUrdfFile(input=paths['input'], robotName=paths['robotName'],
+                                           initTranslation=paths['translation'], initRotation=paths['rotation'])
+            f = open(paths['output'], 'w')
+            f.write(robot_string)
+            f.close()
             for expected in paths['expected']:
                 self.assertTrue(fileCompare(expected.replace('expected', 'results'), expected),
                                 msg='Expected result mismatch when exporting "%s"' % paths['input'])

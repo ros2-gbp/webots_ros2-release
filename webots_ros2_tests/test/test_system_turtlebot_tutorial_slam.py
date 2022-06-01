cbc2f93775e3f1d09665a6620c31e38057618ffe
ad-daniel <daniel.dias@epfl.ch>
2022-06-01 21:56:42 +0200

    Rebase from 'upstream'

--- webots_ros2_tests/test/test_system_turtlebot_tutorial_slam.py
+++ webots_ros2_tests/test/test_system_turtlebot_tutorial_slam.py
@@ -32,10 +32,13 @@ from webots_ros2_tests.utils import TestWebots, initialize_webots_test
 @pytest.mark.rostest
 def generate_test_description():
     initialize_webots_test()
-    # If ROS_DISTRO is rolling or galactic, skip the test as some required packages are missing (cf. ci_after_init.bash)
-    if 'ROS_DISTRO' in os.environ and \
-            (os.environ['ROS_DISTRO'] == 'rolling' or os.environ['ROS_DISTRO'] == 'galactic'):
-        pytest.skip('ROS_DISTRO is rolling or galactic, skipping this test')
+    # If testing in GitHub CI, skip the test as RViz might crash
+    if 'CI' in os.environ and os.environ['CI'] == '1':
+        pytest.skip('RViz might crash in CI, skipping this test')
+
+    # If ROS_DISTRO is rolling or humble, skip the test as some required packages are missing (cf. ci_after_init.bash)
+    if 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] != 'foxy' and os.environ['ROS_DISTRO'] != 'galactic':
+        pytest.skip('ROS_DISTRO is rolling or humble, skipping this test')
 
     # Webots
     turtlebot_webots = IncludeLaunchDescription(

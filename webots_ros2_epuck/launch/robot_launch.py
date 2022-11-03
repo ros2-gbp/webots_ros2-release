e06b96b948085bed5da9222c1bd950ed4ce2a5ca
ygoumaz <yannick.goumaz@gmail.com>
2022-11-03 09:18:54 +0100

    Rebase from 'upstream'

--- webots_ros2_epuck/launch/robot_launch.py
+++ webots_ros2_epuck/launch/robot_launch.py
@@ -26,24 +26,14 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
-from webots_ros2_driver.utils import get_wsl_ip_address, is_wsl
+from webots_ros2_driver.utils import controller_url_prefix
 
 
-def generate_launch_description():
+def get_ros2_nodes(*args):
     package_dir = get_package_share_directory('webots_ros2_epuck')
-    world = LaunchConfiguration('world')
     robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'epuck_webots.urdf')).read_text()
     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2_control.yml')
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-
-    webots = WebotsLauncher(
-        world=PathJoinSubstitution([package_dir, 'worlds', world])
-    )
-
-    controller_url = 'tcp://' + get_wsl_ip_address() + ':1234/' if is_wsl() else ''
-
-    ros2_supervisor = Ros2SupervisorLauncher()
-
     controller_manager_timeout = ['--controller-manager-timeout', '50']
     controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
 
@@ -79,7 +69,7 @@ def generate_launch_description():
         package='webots_ros2_driver',
         executable='driver',
         output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': controller_url + 'epuck'},
+        additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix() + 'epuck'},
         parameters=[
             {'robot_description': robot_description,
              'use_sim_time': use_sim_time,
@@ -114,6 +104,35 @@ def generate_launch_description():
         arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
     )
 
+    return [
+        joint_state_broadcaster_spawner,
+        diffdrive_controller_spawner,
+        robot_state_publisher,
+        epuck_driver,
+        footprint_publisher,
+        epuck_process,
+    ]
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('webots_ros2_epuck')
+    world = LaunchConfiguration('world')
+
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world])
+    )
+
+    ros2_supervisor = Ros2SupervisorLauncher()
+
+    # The following line is important!
+    # This event handler respawns the ROS 2 nodes on simulation reset (supervisor process ends).
+    reset_handler = launch.actions.RegisterEventHandler(
+        event_handler=launch.event_handlers.OnProcessExit(
+            target_action=ros2_supervisor,
+            on_exit=get_ros2_nodes,
+        )
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'world',
@@ -122,18 +141,20 @@ def generate_launch_description():
         ),
         webots,
         ros2_supervisor,
-        joint_state_broadcaster_spawner,
-        diffdrive_controller_spawner,
-        robot_state_publisher,
-        epuck_driver,
-        footprint_publisher,
-        epuck_process,
 
         # This action will kill all nodes once the Webots simulation has exited
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+                on_exit=[
+                    launch.actions.UnregisterEventHandler(
+                        event_handler=reset_handler.event_handler
+                    ),
+                    launch.actions.EmitEvent(event=launch.events.Shutdown())
+                ],
             )
-        )
-    ])
+        ),
+
+        # Add the reset event handler
+        reset_handler
+    ] + get_ros2_nodes())

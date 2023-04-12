0c107401cd67119c5b562e67a5d4e695f35afc2b
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-04-12 16:07:57 +0200

    Rebase from 'upstream'

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -14,7 +14,7 @@ NIGHTLY_DATE=`date --date="${LAST_NIGHTLY_DAY_OLD} day ago" +"%-d_%-m_%Y"`
 WEBOTS_NIGHTLY_VERSION="nightly_${NIGHTLY_DATE}"
 
 apt update
-apt install -y wget dialog apt-utils psmisc lsb-release
+apt install -y wget dialog apt-utils psmisc lsb-release git
 wget https://github.com/cyberbotics/webots/releases/download/${WEBOTS_NIGHTLY_VERSION}/webots_${WEBOTS_RELEASE_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
 
@@ -26,9 +26,19 @@ if [[ $(lsb_release -rs) == "22.04" && ${WEBOTS_RELEASE_VERSION} == "2022a" ]]; 
   mv /tmp/openssl-1.1/* /usr/local/webots/lib/webots/
 fi
 
-# The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
-if [[ "${ROS_DISTRO}" == "foxy" ]]; then
-    apt install -y ros-${ROS_DISTRO}-turtlebot3-cartographer ros-${ROS_DISTRO}-turtlebot3-navigation2
+# The following packages are not available in the ROS 2 Rolling distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
+if [[ "${ROS_DISTRO}" != "rolling" ]]; then
+    apt install -y ros-${ROS_DISTRO}-nav2-bringup
+
+    # Turtlebot3 must be installed from sources to make RViz optional
+    mkdir -p /root/turtlebot_ws/src
+    git clone -b ${ROS_DISTRO}-devel https://github.com/cyberbotics/turtlebot3.git /root/turtlebot_ws/src
+fi
+
+# TODO: Revert once the https://github.com/ros-planning/navigation2/issues/3033 issue is fixed.
+# Fast-DDS is not working properly with the Nav2 package on Humble. Using Cyclone DDS instead.
+if [[ "${ROS_DISTRO}" == "humble" ]]; then
+    apt install -y ros-${ROS_DISTRO}-rmw-cyclonedds-cpp
 fi
 
 # Setup Qt plugins for RViz (can be used once RViz does not randomly crash anymore in GitHub CI).
